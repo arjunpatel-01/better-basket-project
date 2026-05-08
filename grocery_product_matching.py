73cf9f821ae58162ecdcca1ab9fcf151f429adb7
Arjun Patel <arjun.n.patel23@gmail.com>
2026-05-08 01:04:04 -0500

    Update threshold 2

--- grocery_product_matching.py
+++ grocery_product_matching.py
@@ -138,7 +138,7 @@ for i, row_a in enumerate(records_a):
     score = similarities[i][0]
     candidate_b = records_b[top_match_idx]
 
-    if score >= 0.80:
+    if score >= 0.90:
         final_matches.append((row_a["item_id"], candidate_b["item_id"]))
 
 # DEBUG
